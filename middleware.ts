64448adcfd4b4d3d4be2060c7617db1c5a305d52
editionrenn <editionrenn@gmail.com>
2025-08-13 15:08:44 -0500

    middleware update for authMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,9 @@
-// middleware.ts (Clerk v4)
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/pricing', '/sign-in(.*)'],
-  // Don't auth-protect Stripe webhooks:
-  ignoredRoutes: ['/api/stripe/webhook'],
+  publicRoutes: ['/', '/pricing', '/sign-in(.*)', '/api/stripe/(.*)'],
 });
 
 export const config = {
-  matcher: [
-    // exclude _next and all files with an extension
-    '/((?!_next|.*\\..*).*)',
-    // include API routes
-    '/api/(.*)',
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
